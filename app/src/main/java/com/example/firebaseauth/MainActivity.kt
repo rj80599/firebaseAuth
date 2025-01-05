6ecee66e6c71702ecf9bee1690880d66b4073165
Rajbir <rj80599@gmail.com>
2025-01-05 18:02:43 +0530

    init project

---
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.firebaseauth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.firebaseauth.ui.theme.FirebaseAuthTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val authViewModel: AuthViewModel by viewModels()
+        setContent {
+            FirebaseAuthTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyAppNavigation(modifier =  Modifier.padding(innerPadding),authViewModel = authViewModel)
+                }
+            }
+        }
+    }
+}
